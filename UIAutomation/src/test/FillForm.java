a3b96ec039790b659d36a368b7c636573b44617b
Fredrick-Onyango <38939475+Fredrick-Onyango@users.noreply.github.com>
2019-07-18 09:57:54 +0300

    Update FillForm.java

--- UIAutomation/src/test/FillForm.java
+++ UIAutomation/src/test/FillForm.java
@@ -7,9 +7,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
+//import org.openqa.selenium.support.ui.ExpectedConditions;
+//import org.openqa.selenium.support.ui.Wait;
+//import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 /**
@@ -19,7 +19,7 @@ import org.testng.annotations.Test;
 @Test
 public class FillForm {
 	WebDriver driver;
-	    //Set driver		
+	        //Set driver		
 		@Test(priority = 1)
 		public void OpenBrowser() throws InterruptedException{
 			System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir") + "\\resources\\chromedriver.exe");
@@ -30,7 +30,8 @@ public class FillForm {
 		}
 		//Verify Homepage Title
 		@Test(priority = 2)
-		public void VerifyHomepageTitle() throws InterruptedException {
+		public void VerifyHomepageTitle() throws InterruptedException 
+		       {
 			String expectedPageTitle = "Filling Out Forms - Ultimate QA";
 			String actualPageTitle = "";
 			System.out.println(expectedPageTitle);
@@ -44,26 +45,25 @@ public class FillForm {
 			}
 			Thread.sleep(10);
 		}
-		//Fill Form
-			@Test(priority = 3)
-			public void FillForms() throws InterruptedException
-			{
-				//Name
+		//Fill Form & submit
+		@Test(priority = 3)
+		public void FillForms() throws InterruptedException
+		     {
+				//Enter Name
 				driver.findElement(By.xpath("//*[@id='et_pb_contact_name_0']")).sendKeys("Fredrick");
-				//Message
+				//Enter Message
 				driver.findElement(By.xpath("//*[@id='et_pb_contact_message_0']")).sendKeys("Testing UI");
-				//Submit
+				//Click Submit
 				driver.findElement(By.xpath("//*[@id='et_pb_contact_form_0']/div[2]/form/div/button")).click();
 				Thread.sleep(2000);
 				//WebDriverWait wait = new WebDriverWait(driver, 15);
 				//wait.until(ExpectedConditions.textToBePresentInElementLocated(By.xpath("//*[@id='et_pb_contact_form_0']/div/p"), "Time left: 7 seconds"));
 			}
 			
-			//Verify success text message
-			@Test(priority = 4)
-			public void VerifySuccessMessage() throws InterruptedException {
-				//WebDriverWait wait = new WebDriverWait(driver, 15);
-				//wait.until(ExpectedConditions.textToBePresentInElementLocated(By.xpath("//*[@id='et_pb_contact_form_0']/div/p"), "Time left: 7 seconds"));
+		//Verify success text message
+		@Test(priority = 4)
+		public void VerifySuccessMessage() throws InterruptedException 
+		       {
 			WebElement element=driver.findElement(By.xpath("//*[@id='et_pb_contact_form_0']/div/p"));
 			//To store the text of a WebElement in a string
 			String text = element.getText();
